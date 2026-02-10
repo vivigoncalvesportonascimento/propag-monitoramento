840fb23d194b13d3be19f640d253655f67008b98
vivigoncalvesportonascimento <vivigoncalvesporto@gmail.com>
2026-02-10 11:10:45 -0300

    atualiza senhas

--- security/make_password_hash.py
+++ security/make_password_hash.py
@@ -3,13 +3,13 @@ import streamlit_authenticator as stauth
 
 # Substitua pelas senhas em claro apenas LOCALMENTE para gerar os hashes
 senhas = {
-    "admin":   "SENHA_DO_ADMIN",
-    "pmmg":    "SENHA_DA_PMMG",
-    "cbmmg":   "SENHA_DA_CBMMG",
-    "der":     "SENHA_DO_DER",
-    "seinfra": "SENHA_DA_SEINFRA",
-    "pcmg":    "SENHA_DA_PCMG",
-    "sejusp":  "SENHA_DA_SEJUSP",
+    "admin":   "propag_seplag,
+    "pmmg":    "propag_pmmg",
+    "cbmmg":   "propag_cbmmg",
+    "der":     "propag_der",
+    "seinfra": "propag_seinfra",
+    "pcmg":    "propag_pcmg",
+    "sejusp":  "propag_sejusp",
 }
 
 hashes = stauth.Hasher(list(senhas.values())).generate()
